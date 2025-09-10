8037b63432a27de1884549d7a747b93a145efa00
merediano <merediano+learn@outlook.com>
2025-09-10 00:45:45 -0400

    initial labs

---
+++ providers.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_version = ">= 1.10.6" # Replace with your installed version
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
