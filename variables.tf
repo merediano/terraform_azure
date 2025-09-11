4223b26d81c6f23cf6fd6a8581d85d7a7750d814
merediano <merediano+learn@outlook.com>
2025-09-10 21:44:51 -0400

    lab_04 update

--- variables.tf
+++ variables.tf
@@ -15,3 +15,16 @@ variable "location" {
   type        = string
   default     = "canadacentral"
 }
+
+# Subnet Variables
+variable "web_subnet_cidr" {
+  description = "CIDR block for web subnet"
+  type        = string
+  default     = "10.0.1.0/24"
+}
+
+variable "app_subnet_cidr" {
+  description = "CIDR block for app subnet"
+  type        = string
+  default     = "10.0.2.0/24"
+}
